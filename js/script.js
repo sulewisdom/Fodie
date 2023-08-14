8f3acbd2e9029097cbc5b01b6a51b3224496ef24
Sulewisdom <92806800+sulewisdom@users.noreply.github.com>
2023-08-14 23:10:20 +0100

    styled form

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ var swiper = new Swiper(".home-slider", {
   });
 
   var swiper = new Swiper(".review-slider", {
-    spaceBetween: 30,
+    spaceBetween: 20,
     centeredSlides: true,
     autoplay: {
       delay: 7500,
@@ -43,5 +43,19 @@ var swiper = new Swiper(".home-slider", {
     },
 
    loop:true,
+   breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640:{
+      slidesPerView: 2,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    1024:{
+      slidesPerView: 3,
+    },
+   },
     
   });
